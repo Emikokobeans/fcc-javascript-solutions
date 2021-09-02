a8f90e7728133b1df34705256b7b0ba60dff7831
Emi <e.wong@hotmail.co.uk>
2021-09-02 11:28:07 +0100

    complete no.3 of projects

--- javascript-algorithms-and-data-structures/projects/03-caesars-cipher.js
+++ javascript-algorithms-and-data-structures/projects/03-caesars-cipher.js
@@ -1,5 +1,15 @@
 function rot13(str) {
-  return str;
+  const arr = str.split('').map((char) => {
+    if (/[a-z]/i.test(char)) {
+      if (char.charCodeAt(0) + 13 >= 91) {
+        return String.fromCharCode(65 + (13 - (91 - char.charCodeAt(0))));
+      } else {
+        return String.fromCharCode(char.charCodeAt(0) + 13);
+      }
+    }
+    return char;
+  });
+  return arr.join('');
 }
 
 module.exports = rot13;
